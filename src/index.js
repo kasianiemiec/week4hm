d5cc33b01c729f57af9e3df597864a79691e39e0
kasianiemiec <97481231+kasianiemiec@users.noreply.github.com>
2022-02-06 17:36:53 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let now = new Date();
+let h4 = document.querySelector("h4");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+h4.innerHTML = `${day} ${hours}:${minutes}`;
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityInput.value}`;
+}
+let changeButton = document.querySelector("#search-city");
+changeButton.addEventListener("submit", searchCity);
